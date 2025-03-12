e35e1d2c638d9efac57fdbe41258fa4f9c019acf
Q-alo <dominhquan.cntt@gmail.com>
2025-03-12 13:03:07 +0700

    First modifying btn

--- assets/js/sip.js
+++ assets/js/sip.js
@@ -3,8 +3,10 @@ window.addEventListener("scroll", function () {
     if (window.scrollY > 50) {
         // Change the value as needed
         header.classList.add("scrolled");
+        header.classList.add("p-3");
     } else {
         header.classList.remove("scrolled");
+        header.classList.remove("p-3");
     }
 });
 
