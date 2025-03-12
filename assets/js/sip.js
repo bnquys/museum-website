4abc02d91eb43e9a0fee6fadc8b2d6f8640e95f6
Q-alo <dominhquan.cntt@gmail.com>
2025-03-12 16:39:36 +0700

    fist commit in buy-tickets branch

---
+++ assets/js/sip.js
@@ -0,0 +1,11 @@
+window.addEventListener("scroll", function () {
+    const header = document.querySelector(".header");
+    if (window.scrollY > 50) {
+        // Change the value as needed
+        header.classList.add("scrolled");
+        header.classList.add("p-3");
+    } else {
+        header.classList.remove("scrolled");
+        header.classList.remove("p-3");
+    }
+});
