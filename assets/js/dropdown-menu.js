dde12b531441f09878dd6735c1e891df19854048
Q-alo <dominhquan.cntt@gmail.com>
2025-03-19 18:42:05 +0700

    Home Done (Fix Menu)

--- assets/js/dropdown-menu.js
+++ assets/js/dropdown-menu.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
 	var listItems = $("#navbarSupportedContent li").clone();
 	listItems.addClass("dropdown-item");
-	listItems.find("a").addClass("ps-3 fs-4 drop-menu");
+	listItems.find("a").removeClass("px-3");
+	listItems.find("a").addClass("fs-4 drop-menu d-block text-center p-3");
 	$("#dropdown-menu").append(listItems);
 
 });
