ca8379f2be2f8c276bb9cc86d5bf91fc96bb75d0
Q-alo <dominhquan.cntt@gmail.com>
2025-04-17 11:43:28 +0700

    DONE Contact

--- components/signup.php
+++ components/signup.php
@@ -151,7 +151,7 @@
 				type="email"
 				name="email"
 				id="email"
-				class="form-control <?= isset($emailError) ? 'is-invalid' : '' ?>"
+				class="form-control <?= !isset($emailError) ? 'is-invalid' : '' ?>"
 				value="<?= $email?>"
 				required
 			/>
@@ -174,7 +174,7 @@
 			>
 			<input
 				type="text"
-				class="form-control <?= isset($usernameError) ? 'is-invalid' : '' ?>"
+				class="form-control <?= !isset($usernameError) ? 'is-invalid' : '' ?>"
 				id="username"
 				name="username"
 				value="<?= $username?>"
@@ -188,7 +188,7 @@
 			<input
 				type="password"
 				id="password"
-				class="form-control <?= isset($passwordError) ? 'is-invalid' : '' ?>"
+				class="form-control <?= !isset($passwordError) ? 'is-invalid' : '' ?>"
 				aria-describedby="passwordHelpBlock"
 				name="password"
 				required
@@ -201,7 +201,7 @@
 			<input
 				type="password"
 				id="confirm-pass"
-				class="form-control <?= isset($passwordError) ? 'is-invalid' : '' ?>"
+				class="form-control <?= !isset($passwordError) ? 'is-invalid' : '' ?>"
 				name="confirmPass"
 				aria-describedby="passwordHelpBlock"
 				required
