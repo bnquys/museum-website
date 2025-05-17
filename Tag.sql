8dd55092fa996d03eb51c2e0f8b9c777c7ca6cab
Quys <mynameisquy102@gmail.com>
2025-05-17 14:13:01 +0700

    Update Tag.sql

--- Tag.sql
+++ Tag.sql
@@ -1,60 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: May 17, 2025 at 02:19 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `museum`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Tag`
---
-
-CREATE TABLE `Tag` (
-  `Id` varchar(20) NOT NULL,
-  `Name` text DEFAULT NULL,
-  `IsShow` tinyint(1) DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
---
--- Dumping data for table `Tag`
---
-
-INSERT INTO `Tag` (`Id`, `Name`, `IsShow`) VALUES
-('TAG01', 'History', 1),
-('TAG02', 'Art', 1),
-('TAG03', 'Culture', 1),
-('TAG04', 'Science', 1),
-('TAG05', 'Architecture', 1);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `Tag`
---
-ALTER TABLE `Tag`
-  ADD PRIMARY KEY (`Id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO Tag (Id, Name, IsShow) VALUES
+('TAG06', 'Technology', 1),
+('TAG07', 'Photography', 1),
+('TAG08', 'Nature', 1),
+('TAG09', 'Education', 1),
+('TAG10', 'Innovation', 1);
